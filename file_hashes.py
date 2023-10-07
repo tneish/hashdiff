a727e9bdfa376a3c605fdd2a600d2dff3f2d46b3
T <t@us>
2023-10-07 22:11:01 +0200

    Read 100MB at a time (avoid out-of-memory for large files). Ignore 0B files.

--- file_hashes.py
+++ file_hashes.py
@@ -2,7 +2,11 @@
 
 import glob
 import hashlib
+import mmap
 import sys
+import os
+
+MAX_STRIDE = 100*1024*1024  # 100MB
 
 def usage():
     print(str(sys.argv[0]) + ' <path> <outfile>')
@@ -11,9 +15,22 @@ def hashfiles(filelist):
     hashdict = {}
     for f in filelist:
         try:
+            file_size = os.path.getsize(f)
+            if file_size is 0: continue
             with open(f, 'rb') as fobj:
                 print('Adding ' + str(f))
-                h = hashlib.sha1(fobj.read()).hexdigest()
+                h = hashlib.sha1()
+                fptr = 0
+                while fptr < file_size:
+                    stride = min(file_size - fptr, MAX_STRIDE)
+                    with mmap.mmap(fobj.fileno(), 
+                                   stride, 
+                                   access=mmap.ACCESS_READ,
+                                   offset=fptr) as mm:
+                        mm.madvise(mmap.MADV_SEQUENTIAL)
+                        h.update(mm.read())
+                    fptr += stride
+                h = h.hexdigest()
                 try:
                     hashdict[h].append(str(f))
                 except KeyError:
@@ -23,10 +40,10 @@ def hashfiles(filelist):
     return hashdict
 
 def writecsv(hashlist, filename):
-    with open(filename, 'w') as fptr:
+    with open(filename, 'w') as fobj:
         for h in hashlist.keys():
-            fptr.write(h + ''.join([',' + f for f in hashlist[h]]))
-            fptr.write('\n')
+            fobj.write(h + ''.join([',' + f for f in hashlist[h]]))
+            fobj.write('\n')
     return
 
 
